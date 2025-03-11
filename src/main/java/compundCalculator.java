54a7b83536b3f7b509687a8752d941c3ef8998d0
admin <abdoabdoe@gmail.com>
2025-03-11 21:10:23 +0200

    Playing with Java again

--- src/main/java/compundCalculator.java
+++ src/main/java/compundCalculator.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class compundCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double principle;
+        double rate;
+        int timesCompunded;
+        int years;
+        double amount;
+
+        System.out.println("Enter the priciple amount: ");
+        principle = input.nextDouble();
+
+        System.out.println("Enter the interest rate (in %): ");
+        rate = input.nextDouble() /100;
+
+        System.out.println("Enter the number of times compunded per year: " );
+        timesCompunded = input.nextInt();
+
+        System.out.println("Enter the number of years: ");
+        years = input.nextInt();
+
+        amount = principle * Math.pow(1 + rate / timesCompunded, timesCompunded * years);
+
+        System.out.printf("The amount after %d years is %f", years, amount);
+
+
+        input.close();
+
+    }
+
 }
